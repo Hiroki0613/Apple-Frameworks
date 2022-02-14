c5892b709f4f5f2375ce05fec2e4ab610142c9aa
Hiroki Kondo <kondo.matr02@gmail.com>
2022-02-14 14:10:18 +0900

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by 近藤宏輝 on 2022/02/14.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
