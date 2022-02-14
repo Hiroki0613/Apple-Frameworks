783f523f0b1fa073fce693eb2eb71570e0a4df97
Hiroki Kondo <kondo.matr02@gmail.com>
2022-02-14 16:04:15 +0900

    トップ画面のアイコン一覧が完成

---
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,73 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by 近藤宏輝 on 2022/02/14.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks) { framework in
+                        FrameworkTitleView(framework: framework)
+                    }
+                }
+            }
+            .navigationTitle("🍎 Frameworks")
+        }
+
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+            .preferredColorScheme(.dark)
+    }
+}
+
+struct AppIcon: View {
+    
+    var imageName: String
+    var text: String
+    
+    var body: some View {
+        VStack {
+            Image(imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100, height: 100)
+            Text(text)
+                .font(.system(size: 18, weight: .bold))
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct FrameworkTitleView: View{
+
+    let framework: Framework
+    
+    var body: some View {
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+        .padding()
+    }
+}
