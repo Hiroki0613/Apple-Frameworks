e46dacccc53e4807c0c28b614f4d5b9b7947b608
Hiroki Kondo <kondo.matr02@gmail.com>
2022-02-14 22:26:36 +0900

    safariViewを実装

---
+++ Apple-Frameworks/SafariView.swift
@@ -0,0 +1,22 @@
+//
+//  SafariView.swift
+//  Apple-Frameworks
+//
+//  Created by 近藤宏輝 on 2022/02/14.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+    }
+    
+}
