6f59b462f1ad6a9bd5bee57a1b6f0e2982273632
Hiroki Kondo <kondo.matr02@gmail.com>
2022-02-14 19:56:43 +0900

    画面遷移できる様に実装

---
+++ Apple-Frameworks/FrameworkGridViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworkGridViewModel.swift
+//  Apple-Frameworks
+//
+//  Created by 近藤宏輝 on 2022/02/14.
+//
+
+import SwiftUI
+
+final class FrameworkGridViewModel: ObservableObject {
+    var selectedFramework: Framework? {
+        didSet {
+            isShowingDetailView = true
+        }
+    }
+    @Published var isShowingDetailView = false
+}
